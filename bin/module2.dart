feb7b4a7522c9ab1da3904e5d388973807dc5270
Md. Abdulla Al Mamun <mamunkp777@gmail.com>
2024-02-23 19:19:22 +0600

    giugiuh

---
+++ bin/module2.dart
@@ -0,0 +1,10 @@
+main(){
+ int firstNum = 10;
+ double secondNum =3.1416;
+ double result =firstNum.toDouble() + secondNum ;
+
+ int resultTwo =firstNum + secondNum.toInt() ;
+
+ print(resultTwo);
+ print(result.toStringAsFixed(2));
+}
