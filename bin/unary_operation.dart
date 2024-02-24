a7e3cfa4f9116b71b4adf776625cba866496084a
Md. Abdulla Al Mamun <mamunkp777@gmail.com>
2024-02-24 11:31:38 +0600

    unary_operation.dart

---
+++ bin/unary_operation.dart
@@ -0,0 +1,30 @@
+main(){
+  /*var x = 20;
+  print(x++);//x=x+1
+  print(x);
+
+  var y = 25;
+  print(++y);
+  print(y);
+
+  var a = 30;
+  print(a--);//a=a-1
+  print(a);
+
+  var b = 50;
+  print(--b);
+  print(b);*/
+  double a =10;
+  var b =15;
+  a += 10; //a=a+10;
+  print(a);
+  b -= 20;   //b=b-20;
+  print(b);
+  a *= 5;     //a=a*5;
+  print(a);
+  a/=2;   //a=a/2;
+ print(a);
+  a%=2; //a=a%2;
+  print(a);
+
+}
