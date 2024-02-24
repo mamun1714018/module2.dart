d34f4dcf6bd11b0e71119d613e9e9c4b1ed424e1
Md. Abdulla Al Mamun <mamunkp777@gmail.com>
2024-02-24 12:28:18 +0600

    if_else.dart

---
+++ bin/if_else.dart
@@ -0,0 +1,21 @@
+main (){
+  //jodi rickshaw pai tahole rickahaw te kore jbo
+  //jodi na pai hete jbo
+  bool gotRickshaw = true;
+  if(gotRickshaw == true){
+    print('rickshaw te bajare jabo');
+  }
+  else{
+    print('hete bajare jbo');
+  }
+  //nahole hete jbo
+  double taka = 50.25;
+  if(taka>=50)
+    {
+      print('Pizza');
+    }
+  else {
+    print('Banana');
+  }
+
+}
