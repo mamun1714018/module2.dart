dad234ec5eda7f6beb823946fe4264d6a916e8b8
Md. Abdulla Al Mamun <mamunkp777@gmail.com>
2024-02-24 10:08:07 +0600

    constantNew

---
+++ bin/operator.dart
@@ -0,0 +1,21 @@
+main(){
+  int a = 12+12;
+  int b = 12-5;
+  int c = 5*5;
+  double d =25/7;
+  int e =27%5;
+  print(a);
+  print(b);
+  print(c);
+  print(d.toStringAsFixed(3));
+  print (e);
+  print(a++);
+  print(a);
+  print(a--);
+  print(a);
+  print(++a);
+  print (--a);
+  //Unary operator
+
+
+}
